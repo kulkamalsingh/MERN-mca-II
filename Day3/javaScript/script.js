e965a5e0ee995b50b3286c8fa391c1216b462051
Kulkamal Singh <kulkamalsingh377@gmail.com>
2024-04-04 16:30:01 +0530

    upto Day3

---
+++ Day3/javaScript/script.js
@@ -0,0 +1,151 @@
+// var message = "Hello Wolrd";
+
+// var age = 18;
+// age = "Done";
+
+// console.log("message");
+// console.log("age");
+
+// function env() {
+//   console.log(message);
+
+//   var message = "Hello";
+
+//   console.log(message);
+
+//   message = "else";
+
+//   console.log(message);
+// }
+
+// env();
+
+// function env() {
+//   for (var i = 0; i < 5; i++) {
+//     console.log("---", i);
+//   }
+//   console.log(i);
+// }
+
+// let message = "kamal";
+
+// console.log(message);
+
+// const message = "hello";
+
+// console.log(message);
+
+// message = "hello";
+
+// console.log(message);
+
+// let age = 20;
+
+// console.log(typeof age);
+
+// let arr = ['a', 'b', 'c', 'd'];
+// arr[0] =
+
+// let str = "12";
+// let age = 20;
+// let ans = age - str;
+// console.log(ans);
+
+// let a = "12";
+// let b = 12;
+// if (a == b) {
+//   console.log("yes");
+// }
+
+// const str = "khan";
+
+// const usrName = "my name is :";
+
+// const ans = ` ${usrName} ${str}`;
+// console.log(ans);
+
+// function print(str = 'nothing is here') {
+//   console.log(str);
+// }
+// const s = "Hello pappu";
+// print(s);
+
+// function sum(a, b) {
+//   if (a && b) {
+//     console.log(a + b);
+//   } else if (a) {
+//     console.log("a");
+//   } else {
+//     console.log(0);
+//   }
+// }
+// sum();
+// sum(2);
+// sum(2, 3);
+
+// function declaration
+
+// function print() {
+//   console.log("---");
+// }
+
+// // function assignment (named)
+
+// const a = function print() {
+//   console.log("---");
+// };
+
+// // function assignment (anonymous)
+
+// const b = function () {
+//   console.log("---");
+// };
+
+// // function assignment (arrow)
+
+// let c = () => {
+//   console.log("---");
+// };
+
+// print();
+// a();
+// b();
+// c();
+
+// let a = 2;
+// switch (a == 1) {
+//   case 1: {
+//     console.log("one");
+//     break;
+//   }
+//   case 2: {
+//     console.log("Two");
+//     break;
+//   }
+//   case 3: {
+//     console.log("Three");
+//     break;
+//   }
+//   default: {
+//     console.log("not 1,2,3");
+//   }
+// }
+
+// const obj = new Object();
+// obj[1] = "one";
+
+// console.log(obj);
+
+const obj = {
+  1: "one",
+  firstname: "ravi",
+  lastname: "singh",
+  12: "twelve",
+};
+
+const fullname1 = obj.firstName + " " + obj.lastname;
+console.log(obj);
+
+const x = prompt("pleshghgruv");
+
+console.log(obj[x]);

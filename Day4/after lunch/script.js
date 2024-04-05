1416f928ab01fd83c79f568771375f09db642248
Kulkamal Singh <kulkamalsingh377@gmail.com>
2024-04-05 14:30:05 +0530

    undertanding event

--- Day4/after lunch/script.js
+++ Day4/after lunch/script.js
@@ -1,8 +1,18 @@
-const res = document.querySelector("div");
+// const res = document.querySelector("div");
 
-const c = document.querySelector("p");
+// const c = document.querySelector("p");
+// const c = document.querySelector("p:nth-of-type(2)");
 
-console.log(res);
+// console.log(res);
 
 // res.remove();
-res.removeChild(c);
+// res.removeChild(c);
+
+function getInfo() {
+  const d = document.querySelector("div");
+  const el = document.createElement("p");
+  el.innerText = "my name is kamal";
+  d.prepend(el);
+  console.log("Btn clicked");
+  d.prepend(hi);
+}

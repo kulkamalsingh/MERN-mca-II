6b152f4441943e23bc12e009baa0850da898b4ec
Kulkamal Singh <kulkamalsingh377@gmail.com>
2024-04-05 16:09:19 +0530

    prepend append

--- Day4/after lunch/script.js
+++ Day4/after lunch/script.js
@@ -8,11 +8,18 @@
 // res.remove();
 // res.removeChild(c);
 
-function getInfo() {
-  const d = document.querySelector("div");
-  const el = document.createElement("p");
-  el.innerText = "my name is kamal";
-  d.prepend(el);
-  console.log("Btn clicked");
-  d.prepend(hi);
-}
+// function getInfo() {
+//   const d = document.querySelector("div");
+//   const el = document.createElement("p");
+//   el.innerText = "my name is kamal";
+//   d.prepend(el);
+//   console.log("Btn clicked");
+//   d.prepend(hi);
+// }
+
+// function getInfo(e) {
+//   console.log(e);
+//   e.taget.class = "abc";
+//   e.target.setAttribute("style", "color:red");
+//   e.target.setAttribute("style", "background-color:red");
+// }

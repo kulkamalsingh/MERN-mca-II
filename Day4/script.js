723d8a8833479ee389191153b3f6ba98e93858d9
Kulkamal Singh <kulkamalsingh377@gmail.com>
2024-04-05 10:40:26 +0530

    obj & Array

---
+++ Day4/script.js
@@ -0,0 +1,66 @@
+// let a = new String("Hello");
+// let b = "Hello";
+
+// console.log(a);
+// console.log(b);
+
+// const obj = {
+//   name: "kamal",
+//   lastName: "Singh",
+// };
+
+// console.log(obj);
+// document.write(obj);
+
+// const obj = {
+//   name: "kamal",
+//   lastName: "Singh",
+// };
+
+// obj.age = 20;
+// console.log(obj);
+
+// const obj = {
+//   name: "kamal",
+//   lastName: "Singh",
+// };
+
+// obj.name = 20;
+// console.log(obj);  we can't write again = in front of obj but wehave to focus on key value pair
+
+// let arr = [1, 2, 3, 4];
+// arr = [3.4, 5];
+// console.log(arr);
+
+// const arr = [1, 2, 3, 4];
+// arr[10] = 20;
+// arr.push(12);
+// console.log(arr);
+// console.log(typeof obj);
+// console.log(typeof arr);
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+// const obj = {
+//   name: "kamal",
+//   lastName: "Singh",
+// };
+
+// const arr = [1, 2, 3, 4];
+
+// console.log(Array.isArray(obj));
+// console.log(Array.isArray(arr));
+// console.log(Array.isArray(null));
+// console.log(Array.isArray(undefined));
+
+// function checkIfObject(x) {
+//   if (Array.isArray(x)) {
+//     console.log("not object");
+//   } else if (typeof x == "object") {
+//     console.log("object");
+//   } else {
+//     console.log("not object");
+//   }
+// }
+
+// checkIfObject([]);

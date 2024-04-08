a26f65408edb1f2a793172113970a0b89b072c80
Kulkamal Singh <kulkamalsingh377@gmail.com>
2024-04-08 12:41:07 +0530

    double promises

---
+++ Day5/AsyncJS/script.js
@@ -0,0 +1,42 @@
+// console.log("GEC Start");
+
+// function printPretty() {
+//   console.log("prettyStart");
+//   let ans = 2 + 3;
+//   console.log(ans);
+//   console.log("prettyEnd");
+// }
+// setTimeout(printPretty, 1000);
+
+// console.log("GEC End");
+
+// const btn = document.getElementById("btn");
+// btn.addEventListener("click", printPretty);
+
+// console.log("Start");
+
+// const req = fetch("https://api-football-v1.p.rapidapi.com/v3/timezone");
+// console.log(req);
+
+// req.then(() => console.log("fetched"));
+// console.log("End");
+
+// console.log("start");
+
+// fetch("https://dummyjson.com/products/1")
+//   .then((res) => res.json());
+// res.then((json) => console.log(json));
+
+// console.log("end");
+
+// console.log("start");
+// const req = fetch("https://dummyjson.com/products/1");
+// const res = res.then((res) => res.json());
+// res.then((data) => console.log(data));
+// console.log("end");
+
+console.log("start");
+fetch("https://dummyjson.com/products/1")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+console.log("end");

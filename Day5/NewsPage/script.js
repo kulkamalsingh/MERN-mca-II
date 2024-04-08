5e678763ec993bb9c080ffabf534f220824d9858
Kulkamal Singh <kulkamalsingh377@gmail.com>
2024-04-08 15:36:15 +0530

    updated CSS

--- Day5/NewsPage/script.js
+++ Day5/NewsPage/script.js
@@ -28,27 +28,29 @@ function callAPI() {
     });
 }
 
+const root = document.getElementById("root");
 function renderUI(data) {
-  const root = document.getElementById("root");
-
-  // got the articles from data
   const articles = data.articles;
-  console.log(articles);
 
-  //  single articles from the articles array
-  const ar = articles[2];
+  for (let i = 0; i < articles.length; i++) {
+    const ar = articles[i];
+    const div = document.createElement("div");
+    div.setAttribute("class", "news-card");
 
-  console.log(ar);
+    const h3 = document.createElement("h3");
+    h3.innerText = ar.title;
+    div.appendChild(h3);
 
-  const div = document.createElement("div");
-  const h3 = document.createElement("h3");
-  h3.innerText = ar.title;
-  const img = document.createElement("img");
-  img.src = ar.urlToImage;
-  div.appendChild(h3);
-  div.appendChild(img);
-  // div.innerText = ar.title;
-  root.appendChild(div);
+    const img = document.createElement("img");
+    img.src = ar.urlToImage;
+    div.appendChild(img);
+
+    const p = document.createElement("p");
+    p.innerText = ar.description;
+    div.appendChild(p);
+
+    root.appendChild(div);
+  }
 }
 
 // renderUI();

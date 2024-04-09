e7776b5345ed1f04a07d51e1c63cbb5b8970c009
Kulkamal Singh <kulkamalsingh377@gmail.com>
2024-04-09 10:33:49 +0530

    setTimeout handling promises

--- Day6/script.js
+++ Day6/script.js
@@ -5,15 +5,56 @@
 // }, 0);
 
 // console.log("end");
-setTimeout(() => {
-  console.log("timeout....1..");
-}, 2000);
-console.log("start");
 
-setTimeout(() => {
-  console.log("timeout....2..");
-}, 0);
+// setTimeout(() => {
+//   console.log("timeout....1..");
+// }, 2000);
+// console.log("start");
 
-console.log("intermidiate");
+// setTimeout(() => {
+//   console.log("timeout....2..");
+// }, 0);
 
-console.log("end");
+// console.log("intermidiate");
+
+// console.log("end");
+
+// creating promices
+
+// function createOrder(x, fn) {
+//   console.log(x);
+//   //   fn("kamal123");
+//   setTimeout(() => {
+//     fn("kamal123");
+//   }, 2000);
+// }
+
+// function makePayment(orderID) {
+//   console.log(orderID);
+// }
+
+// createOrder("soap", makePayment); // makePayment giving control to create order, to avoid this we need promises
+
+//*******************JS Promises**********
+// const pr = new Promise((resolve, reject) => {
+//   // in IF we write logic
+//   //   if (id == "userID123")
+//   if (true) {
+//     setTimeout(() => {
+//       resolve();
+//     }, 5000);
+//   } else {
+//     reject();
+//   }
+// });
+
+// console.log(pr);
+
+const pr = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("apka kaam ho gya hai");
+  }, 1000);
+});
+pr.then((res) => {
+  console.log(res);
+});

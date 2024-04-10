2c890f25c1f2b034d7904ab857faf50b87f9649c
Kulkamal Singh <kulkamalsingh377@gmail.com>
2024-04-10 10:15:10 +0530

    calling myReadMe file

--- Day7/Internal_modules/script.js
+++ Day7/Internal_modules/script.js
@@ -1,4 +1,9 @@
-const fs = require("node:fs");
+// const fs = require("node:fs");
 
-const data = fs.readFileSync("./myReadMe.txt");
+// const data = fs.readFileSync("./myReadMe.txt");
+// console.log(data);
+// console.log(data.toString());
+
+const fs = require("fs");
+const data = fs.readFileSync("./myReadMe.txt", { encoding: "utf-8" });
 console.log(data);

1f23e222a532adecbfab1e54c535196a4f7cd76b
Kulkamal Singh <kulkamalsingh377@gmail.com>
2024-04-28 00:10:36 +0530

    .

--- amazonBackend/routes/productsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const productsController = require("../controllers/productsControlles");
-const productRouter = express.Router();
-
-productRouter.route("/")
-    .get(productsController.getAllProducts)
-    .post(productsController.addProduct)
-module.exports = productRouter;
